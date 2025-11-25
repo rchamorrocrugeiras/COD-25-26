5ab0fb4b24e5000e55d03cb65d1c56e1eb0dce89
René <rchamorrocrugeiras@danielcastelao.org>
2025-11-25 14:13:11 +0100

    pruebas para tablero

--- Hundir la flota/tablero.py
+++ Hundir la flota/tablero.py
@@ -10,3 +10,14 @@ class Tablero:
 
         self.casillas = [[0 for _ in range(columnas)] for _ in range(filas)]
 
+    def mostrar_tablero(self):
+        print("TABLERO")
+        for fila in self.casillas:
+            print(" ".join(str(casilla) for casilla in fila))
+        print("-" * 20)
+
+
+# Pruebas:
+if __name__ == "__main__":
+    tablero = Tablero(5)
+    tablero.mostrar_tablero()
